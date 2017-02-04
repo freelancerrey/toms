8032bcd8135228855c8a1d06153b657ebf8d2bbd
linuxiast <linuxiespot@gmail.com>
2017-02-04 18:45:05 +0800

    Customized Password Reset Email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.guest')
+
+@section('title', 'Reset Password')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+    <div class="jumbotron">
+        <h2 class='text-left' style='margin: 0 0 20px 0;'>RESET PASSWORD</h2>
+        {{ csrf_field() }}
+        <!-- <div role="alert" class="alert alert-danger well-sm text-left"><strong>Opps!</strong> Invalid Email or Password!</div> -->
+        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} text-left">
+            <label for="email">Email</label>
+            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="Email">
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+        </div>
+        <div class="form-group text-left">
+            <button type="submit" class="btn btn-primary">Send Password Reset Link</button>
         </div>
     </div>
-</div>
+</form>
 @endsection
