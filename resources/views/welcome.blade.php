fae65910f9bd505aa49ac0d3f9896b0cdf4588f3
linuxiast <linuxiespot@gmail.com>
2017-02-06 11:20:57 +0800

    set welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,9 +68,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
+                    @if (!Auth::check())
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
                     @endif
@@ -83,7 +81,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="{{ url('/dashboard') }}">Dashboard</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
