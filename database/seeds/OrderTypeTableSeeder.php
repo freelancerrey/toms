12b2bf87abdec59ceb055c304485ce1b3a2510d1
linuxiast <linuxiespot@gmail.com>
2017-02-17 18:36:17 +0800

    added unknown order type in seeder

--- database/seeds/OrderTypeTableSeeder.php
+++ database/seeds/OrderTypeTableSeeder.php
@@ -12,6 +12,7 @@ class OrderTypeTableSeeder extends Seeder
     public function run()
     {
         DB::table('order_types')->insert([
+            ['id' => 0, 'type' => 'Unknown', 'is_active' => 1],
             ['type' => 'Optin Rate Guaranteed', 'is_active' => 1],
             ['type' => 'Direct To Client Page', 'is_active' => 1],
             ['type' => 'Pipeline Subscription', 'is_active' => 1]
